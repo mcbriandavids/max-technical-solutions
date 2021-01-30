be9e6346066683fd7bb53282b27bb7d26d3a7761
Imonisa Brian <mcbriandavids43@gmail.com>
2021-01-30 18:55:21 +0100

    contact page re-updated

--- src/screens/ContactScreen.js
+++ src/screens/ContactScreen.js
@@ -94,17 +94,3 @@ const ContactScreen = () => {
 };
 
 export default ContactScreen;
-<form name='contact' action='/contact' method='post' className='join-form'>
-	<input type='hidden' name='form-name' value='contact' />
-	<label htmlFor='FirstName'>First Name</label>
-	<input type='text' required name='FirstName' placeholder='Your First Name' />
-	<label htmlFor='LastName'>Last Name</label>
-	<input type='text' required name='LastName' placeholder='Your Last Name' />
-	<label htmlFor='Email'>Email</label>
-	<input type='text' required name='Email' placeholder='Your Email' />
-	<label htmlFor='PhoneNumber'>Phone Number</label>
-	<input type='number' required name='Email' placeholder='Your Phone Number' />
-	<button type='submit' className='btn-primary submit-btn'>
-		Submit
-	</button>
-</form>;
